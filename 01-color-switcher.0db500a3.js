e24d9d558bbc6d2837d1183b4aec226a2b9cb5c6
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2023-01-10 22:29:40 +0000

    Deploying to gh-pages from @ olha-sydorenko/goit-js-hw-09@6abed12c6e355ff1007e338a34c9a050ee43f3a5 🚀

---
+++ 01-color-switcher.0db500a3.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled","true"),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled"),e.setAttribute("disabled","true")}))}();
+//# sourceMappingURL=01-color-switcher.0db500a3.js.map
