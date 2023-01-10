3bb82cdf04676a7094bd0105f7fc40911eaa98ac
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2023-01-11 00:24:11 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+
+startBtn.addEventListener('click', ()=> {
+timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+}, 1000);
+startBtn.setAttribute('disabled', 'true');
+});
+
+  stopBtn.addEventListener('click', ()=> {
+      clearInterval(timerId);
+      startBtn.removeAttribute('disabled');     
+  });
+
+  
